479a6642622ac8f62fcaff7f9e586a904e742271
jaidkasahil <88638661+jaidkasahil@users.noreply.github.com>
2022-07-10 18:40:55 +1000

    Initial Commit

---
+++ Million Trades/Million_TradesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Million_TradesApp.swift
+//  Million Trades
+//
+//  Created by Sahil Jaidka on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Million_TradesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
